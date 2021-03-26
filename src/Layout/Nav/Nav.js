5f36c89f16b3f567fdcde27f9797fb37fff64735
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-03-26 21:08:10 +0100

    little className fix

--- src/Layout/Nav/Nav.js
+++ src/Layout/Nav/Nav.js
@@ -11,13 +11,13 @@ const Nav  = ({ setAccessToken }) => {
 
     return ( 
         <div className="nav">
-            <NavLink className="nav-item" activeClassName="is-active" to="/search"><i class="fas fa-search"></i><span>Search</span></NavLink>
-            <NavLink className="nav-item" activeClassName="is-active" to="/me/conversations"><i class="fas fa-envelope"></i><span>Messages</span></NavLink>
-            <NavLink className="nav-item" activeClassName="is-active" to="/me/basket"><i class="fas fa-shopping-basket"></i><span>Baskets</span></NavLink>
-            <NavLink className="nav-item" activeClassName="is-active" to="/me" exact><i class="fas fa-user-circle"></i><span>My profile</span></NavLink>
-            <NavLink className="nav-item" activeClassName="is-active" to="/me/books"><i class="fas fa-book"></i><span>My books</span></NavLink>
+            <NavLink className="nav-item" activeClassName="is-active" to="/search"><i className="fas fa-search"></i><span>Search</span></NavLink>
+            <NavLink className="nav-item" activeClassName="is-active" to="/me/conversations"><i className="fas fa-envelope"></i><span>Messages</span></NavLink>
+            <NavLink className="nav-item" activeClassName="is-active" to="/me/basket"><i className="fas fa-shopping-basket"></i><span>Baskets</span></NavLink>
+            <NavLink className="nav-item" activeClassName="is-active" to="/me" exact><i className="fas fa-user-circle"></i><span>My profile</span></NavLink>
+            <NavLink className="nav-item" activeClassName="is-active" to="/me/books"><i className="fas fa-book"></i><span>My books</span></NavLink>
             <NavLink className="nav-item" activeClassName="is-active" to="/me/pokes"><div className="poke"></div></NavLink>
-            <NavLink className="nav-item log-out" to="/" onClick={handleLogOut}><i class="fas fa-power-off"></i></NavLink>
+            <NavLink className="nav-item log-out" to="/" onClick={handleLogOut}><i className="fas fa-power-off"></i></NavLink>
         </div>
       );
 }
