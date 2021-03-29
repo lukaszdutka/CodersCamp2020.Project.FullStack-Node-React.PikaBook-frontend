05e63f0d9145ca699b3c65cac42253c0b66ac5e0
Gosia Dziewit <gosia.dziewit@gmail.com>
2021-03-29 10:29:28 +0200

    add content and style

--- src/Layout/Footer/Footer.js
+++ src/Layout/Footer/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Footer.scss";
+import pikachu from "../../Assets/Images/pikachu.png"
 
 const Footer  = () => {
     return ( 
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className="about">
+                Pikabook APP is for book lovers. Are you one of them? If so, <b>pick a book</b> you want to read. Then suggest yours you have already read. That's all. <b>Connect with other bookworms!</b>
+                <img src={pikachu} alt="pikachu" />
+            </div>
+            <div className="authors">
+                <b>Created by Mangos'team</b>
+                <ul>
+                    <li>Mentor: Łukasz Dutka</li>
+                    <li>Aleksandra Cypko</li>
+                    <li>Małgorzata Dziewit</li>
+                    <li>Daria Dziubałtowska</li>
+                    <li>Agata Ludwiczyńska</li>
+                    <li>Mariusz Smarż</li>
+                </ul>
+            </div>
+            <div className="github">
+                Application made as part of the <b>CodersCamp2020</b> project.<br />
+                <Link to={{ pathname: "https://github.com/lukaszdutka/CodersCamp2020.Project.FullStack-Node-React.PikaBook-frontend" }} target="_blank">Visit Github for more information.</Link>
+            </div>
+        </footer>
       );
 }
  
