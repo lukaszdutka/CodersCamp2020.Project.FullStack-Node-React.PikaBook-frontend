1ca8902bfd128a4beb9a09777bad4a9cdf4a5625
mariusz-sm <mariusz.smarz@gmail.com>
2021-04-10 19:01:44 +0200

    Correct book fetching

--- src/Views/Me/MeBooks.js
+++ src/Views/Me/MeBooks.js
@@ -1,28 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Book from './Book'
 import AddBook from './AddBook'
 import { searchMyBooks } from '../../API/fetchBooks';
 
-const MeBooks  = (accessToken) => {
-    const [books, setBooks] = useState([]);
+const MeBooks  = ({accessToken}) => {
+    const [myBooks, setMyBooks] = useState([]);
     const [status, setStatus] = useState("My books loading ...");
 
-    const handleLoad = async (e) => {
-      e.preventDefault();
-      setStatus("Searching...");
-      const res = await searchMyBooks({
-        'Authorization': 'Bearer ' + accessToken.token
-      })
-      if (res.error) setStatus(res.error);
-      setBooks(res.books);
-      if (books.length === 0) setStatus("No books found");
-    };
+    // const handleLoad = async (e) => {
+    //   e.preventDefault();
+    //   setStatus("Searching...");
+    //   const res = await searchMyBooks(accessToken)
+    //   console.log("RES" + res)
+    //   if (res.error) setStatus(res.error);
+    //   setBooks(res.books);
+    //   if (books.length === 0) setStatus("No books found");
+    // };
 
-    const bookList = books.map((book) => <Book key={book._id} data={book} />);
+    useEffect(() => {
+      const getBooks = async () => {
+        const res = await searchMyBooks(accessToken);
+        if (res.error) console.log(res.error);
+        if (res.books) setMyBooks(res.books);
+      };
+      getBooks();
+    }, [accessToken]);
+
+    const bookList = myBooks.map((book) => <Book key={book._id} data={book} />);
 
     return ( 
         <div>
-            <div onLoad={handleLoad} >{bookList.length === 0 ? status : bookList}</div>
+            {/* <div onLoad={handleLoad} >{bookList.length === 0 ? status : bookList}</div> */}
+            {bookList}
             <div><AddBook></AddBook></div>
         </div>
       );
