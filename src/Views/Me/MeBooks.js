3727cf830e8d9920c4beb74948c11e5e3a48497c
mariusz-sm <mariusz.smarz@gmail.com>
2021-04-10 21:34:01 +0200

    Remove comments

--- src/Views/Me/MeBooks.js
+++ src/Views/Me/MeBooks.js
@@ -7,21 +7,16 @@ const MeBooks  = ({accessToken}) => {
     const [myBooks, setMyBooks] = useState([]);
     const [status, setStatus] = useState("My books loading ...");
 
-    // const handleLoad = async (e) => {
-    //   e.preventDefault();
-    //   setStatus("Searching...");
-    //   const res = await searchMyBooks(accessToken)
-    //   console.log("RES" + res)
-    //   if (res.error) setStatus(res.error);
-    //   setBooks(res.books);
-    //   if (books.length === 0) setStatus("No books found");
-    // };
-
     useEffect(() => {
       const getBooks = async () => {
         const res = await searchMyBooks(accessToken);
-        if (res.error) console.log(res.error);
-        if (res.books) setMyBooks(res.books);
+        if (res.error) {
+          console.log(res.error);
+          setStatus("No books found.")
+        }
+        if (res.books) {
+          setMyBooks(res.books);
+        }
       };
       getBooks();
     }, [accessToken]);
@@ -30,8 +25,7 @@ const MeBooks  = ({accessToken}) => {
 
     return ( 
         <div>
-            {/* <div onLoad={handleLoad} >{bookList.length === 0 ? status : bookList}</div> */}
-            {bookList}
+            <div >{bookList.length === 0 ? status : bookList}</div>
             <div><AddBook></AddBook></div>
         </div>
       );
