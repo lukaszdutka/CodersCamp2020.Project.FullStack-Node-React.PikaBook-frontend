a5c6267a0c54f154ad8b6465109fd5bc4d99b962
mariusz-sm <mariusz.smarz@gmail.com>
2021-04-12 20:24:24 +0200

    Refactor listProperties 2

--- src/SharedFunctions/listProperties.js
+++ src/SharedFunctions/listProperties.js
@@ -1,5 +1,8 @@
 const listProperties = (list) => {
-  return list.join(", ")
+  return list.map((singleProperty, index) => {
+    if (index < list.length - 1) return singleProperty + ", ";
+    return singleProperty;
+  });
 };
 
 export default listProperties
